9076e6edb62192140595213e93450ef5667e7f7d
bye-bye-kin <65549041+bye-bye-kin@users.noreply.github.com>
2023-07-28 10:13:52 +1000

    Update PanelChanger.cs

--- Assets/Scripts/PanelChanger.cs
+++ Assets/Scripts/PanelChanger.cs
@@ -8,22 +8,27 @@ public class PanelChanger : MonoBehaviour
     void Update()
     {
     }
-        public void OnRightArrow()
-    {
+    public void OnRightArrow()
+     {
         this.transform.position = new Vector2(transform.position.x - 18f, 0);
 
         if(this.transform.position.x < - 54f)
         {
             this.transform.position = new Vector2(0, 0);
         }
-
-    }
+     }
 
    public void OnLeftArrow()
     {
-        this.transform.localPosition = new Vector2(0, 0);
+        //もしパネル０だったらパネル４に移動
+        if (this.transform.position.x > -18f)
+        {
+            this.transform.position = new Vector2(transform.position.x - 54f, 0);
+        }
+        //そうでなければプラス１８
+        else
+        {
+            this.transform.position = new Vector2(transform.position.x + 18f, 0);
+        }
     } 
-
-
-
 }
